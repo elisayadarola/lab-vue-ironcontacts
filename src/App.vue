45256c2952b759960e5414b404cf6c552d0a4899
alexrod94 <alexrod94@gmail.com>
2022-03-17 16:24:45 +0100

    upload files

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
+import contacts from "./contacts.json";
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+
+const myContacts = contacts;
+console.log(myContacts);
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <div v-for="contact in myContacts">{{ contact.name }}</div>
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
